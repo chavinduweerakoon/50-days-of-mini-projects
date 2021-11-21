d1298332f0867a3886d30ead4ea66524e7e6f8c7
chavindu weerakoon <chavinduweerakoon@gmail.com>
2021-11-21 20:39:28 +0530

    displaying keycodes

---
+++ Event key codes/script.js
@@ -0,0 +1,20 @@
+const insert = document.getElementById("insert");
+
+//key listener
+window.addEventListener("keydown", (event) => {
+  insert.innerHTML = `<div class="key">
+    ${event.key === " " ? "Space" : event.key}
+    <small>event.key</small>
+  </div>
+  <div class="key">
+    ${event.keyCode}
+    <small>event.keyCode</small>
+  </div>
+  <div class="key">
+    ${event.code}
+    <small>event.code</small>
+  </div>`;
+});
+window.addEventListener("keydown", (e) => {
+  console.log(e);
+});
